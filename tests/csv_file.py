2360c4c63c5a323b401ba005c3adf3333aad0fb0
Pablo Acosta-Serafini <pmasdev@gmail.com>
2018-04-30 19:16:02 -0400

    Updated copyright notice year

--- tests/csv_file.py
+++ tests/csv_file.py
@@ -1,5 +1,5 @@
 # csv_file.py
-# Copyright (c) 2013-2017 Pablo Acosta-Serafini
+# Copyright (c) 2013-2018 Pablo Acosta-Serafini
 # See LICENSE for details
 # pylint: disable=C0103,C0111,C0302,C0411,C0413
 # pylint: disable=E0611,F0401,R0201,R0204,R0915,W0232
--- tests/write.py
+++ tests/write.py
@@ -1,5 +1,5 @@
 # write.py
-# Copyright (c) 2013-2017 Pablo Acosta-Serafini
+# Copyright (c) 2013-2018 Pablo Acosta-Serafini
 # See LICENSE for details
 # pylint: disable=C0103,C0111,C0302,C0413,E0611,F0401,R0201,R0915,W0232
 
